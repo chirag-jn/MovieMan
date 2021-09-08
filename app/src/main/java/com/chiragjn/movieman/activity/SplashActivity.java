d98332aa9948c54665be75793bde46f18e857b4c
Chirag Jain <jainchg@gmail.com>
2021-09-09 00:34:18 +0530

    modify splash screen

--- app/src/main/java/com/chiragjn/movieman/activity/SplashActivity.java
+++ app/src/main/java/com/chiragjn/movieman/activity/SplashActivity.java
@@ -2,10 +2,14 @@ package com.chiragjn.movieman.activity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
 
 import com.chiragjn.movieman.R;
 
 public class SplashActivity extends BaseActivity {
+
+    private static final int SPLASH_SCREEN_TIME_OUT = 1500;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,11 +20,19 @@ public class SplashActivity extends BaseActivity {
 
     @Override
     void bindView() {
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().hide();
+        }
+
+        setContentView(R.layout.activity_splash);
+
         setUpActivity();
     }
 
     private void setUpActivity() {
-        launchNavigation();
+        new Handler().postDelayed(this::launchNavigation, SPLASH_SCREEN_TIME_OUT);
     }
 
     private void launchNavigation() {
