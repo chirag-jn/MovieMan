153c8680c3b7f02491aeec430f59e5aef3c79725
Chirag Jain <jainchg@gmail.com>
2021-09-09 02:19:44 +0530

    modify home activity to match baseactivity

--- app/src/main/java/com/chiragjn/movieman/activity/HomeActivity.java
+++ app/src/main/java/com/chiragjn/movieman/activity/HomeActivity.java
@@ -1,16 +1,20 @@
 package com.chiragjn.movieman.activity;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
 import com.chiragjn.movieman.R;
 
-public class HomeActivity extends AppCompatActivity {
+public class HomeActivity extends BaseActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        bindView();
+    }
+
+    @Override
+    void bindView() {
         setContentView(R.layout.activity_home);
     }
 }
