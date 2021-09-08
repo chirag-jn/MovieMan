b9f5c48b139ced32ab3abb347afc5411d10cf2ed
Chirag Jain <jainchg@gmail.com>
2021-09-08 23:54:59 +0530

    create splash screen

---
+++ app/src/main/java/com/chiragjn/movieman/activity/HomeActivity.java
@@ -0,0 +1,16 @@
+package com.chiragjn.movieman.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.chiragjn.movieman.R;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
--- app/src/main/java/com/chiragjn/movieman/activity/SplashActivity.java
+++ app/src/main/java/com/chiragjn/movieman/activity/SplashActivity.java
@@ -1,16 +1,31 @@
 package com.chiragjn.movieman.activity;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.chiragjn.movieman.R;
 
-public class SplashActivity extends AppCompatActivity {
+public class SplashActivity extends BaseActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splash);
+
+        bindView();
     }
+
+    @Override
+    void bindView() {
+        setUpActivity();
+    }
+
+    private void setUpActivity() {
+        launchNavigation();
+    }
+
+    private void launchNavigation() {
+        startActivity(new Intent(this, HomeActivity.class));
+        finish();
+    }
+
 }
