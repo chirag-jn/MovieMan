ed7aa93a8c3783925c61e3ed3d1e178a6dd1eaed
Chirag Jain <jainchg@gmail.com>
2021-09-13 15:08:32 +0530

    add stars in movie view

--- app/src/main/java/com/chiragjn/movieman/fragment/viewManager/ViewHolder.java
+++ app/src/main/java/com/chiragjn/movieman/fragment/viewManager/ViewHolder.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.graphics.PorterDuff;
 import android.net.Uri;
 import android.view.View;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -25,16 +26,27 @@ public class ViewHolder extends RecyclerView.ViewHolder {
     private TextView movieYear;
     private TextView movieTitle;
     private Movie movie;
+    private ImageButton star2;
+    private ImageButton star3;
+    private ImageButton star4;
+    private ImageButton star5;
+
 
     public ViewHolder(View view, Context ctx, int type) {
         super(view);
 
-        if(type==0) {
+        if (type == 0) {
             popularityScore = view.findViewById(R.id.movieRating);
             bookmarkBtn = view.findViewById(R.id.movieBookmark);
             posterImg = view.findViewById(R.id.movieBgd);
             bookmarkBtn.setOnClickListener(v -> bookMarkOnClick(ctx));
-        } else if (type==1) {
+
+            star2 = view.findViewById(R.id.star_image2);
+            star3 = view.findViewById(R.id.star_image3);
+            star4 = view.findViewById(R.id.star_image4);
+            star5 = view.findViewById(R.id.star_image5);
+
+        } else if (type == 1) {
             popularityScore = view.findViewById(R.id.movieRating);
             movieYear = view.findViewById(R.id.movieYear);
             movieTitle = view.findViewById(R.id.movieTitle);
@@ -56,20 +68,35 @@ public class ViewHolder extends RecyclerView.ViewHolder {
 
             String avgVote = movie.getRating() + "";
 
-            if(type==0) {
+            if (type == 0) {
                 popularityScore.setText(avgVote);
 
                 if (movie.getPosterPath() != null && movie.getPosterPath().length() > 0) {
                     Uri uri = Uri.parse(Endpoints.IMAGE_URL.concat(movie.getPosterPath()));
                     posterImg.setImageURI(uri);
-                } else {
-//                    TODO: Set placeholder image
                 }
-            } else if(type==1) {
+
+                setStars(Float.parseFloat(avgVote));
+            } else if (type == 1) {
                 popularityScore.setText(avgVote);
                 movieTitle.setText(movie.getTitle());
                 movieYear.setText(movie.getYear());
             }
+        }
+    }
+
+    void setStars(float vote) {
+        if (vote <= 4) {
+            star5.setVisibility(View.GONE);
+        }
+        if (vote <= 3) {
+            star4.setVisibility(View.GONE);
+        }
+        if (vote <= 2) {
+            star3.setVisibility(View.GONE);
+        }
+        if (vote <= 1) {
+            star2.setVisibility(View.GONE);
         }
     }
 
